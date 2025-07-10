4ac9eb0d8171374baf78e5b2a1c6e2836dc58ede
sevan <1453244320@qq.com>
2025-07-10 10:15:24 +0000

    a script used to test MM benchmark

--- precompile.jl
+++ precompile.jl
@@ -1,5 +1,24 @@
-
+using DataFrames
 include("src/PDHCG.jl")
+# ENV["JULIA_PROJECT"] = "pdhcg_env"
+dataset_dir = expanduser("~/QP_datasets/MM_benchmark")
+result_df = DataFrame(
+    dataset = String[],
+    time_cost_sec = Float64[],
+    objective_value = Float64[],
+    outer_iteration_count = Int[],
+    inner_iteration_count = Int[]
+)
+for file in readdir(dataset_dir)
+    if endswith(file, ".QPS")
+        qp = PDHCG.readFile(joinpath(dataset_dir, file))
+        log = PDHCG.pdhcgSolve(qp, gpu_flag=true, warm_up_flag=true, online_precondition_band_dual=nothing, verbose_level=2, time_limit = 600.)
+        time_cost = log.solve_time_sec
+        obj = log.objective_value
+        outer_iter = log.iteration_count
+        inner_iter = log.CG_total_iteration
+        push!(result_df, (file, time_cost, obj, outer_iter, inner_iter))
+        println("Processed file: $file, Time: $time_cost sec, Objective: $obj, Outer Iterations: $outer_iter, Inner Iterations: $inner_iter")
+    end
+end
 
-qp = PDHCG.generateProblem("randomqp", n=100, density=0.001, seed=100)
-PDHCG.pdhcgSolve(qp, gpu_flag=true, warm_up_flag=true)
